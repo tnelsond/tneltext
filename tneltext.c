022fa429ebb1aaa5300696692781bf4d4dd6e860
tnelsond <tnelsond@gmail.com>
2015-08-21 13:55:22 -0500

    Made objects traversable and findable

--- tneltext.c
+++ tneltext.c
@@ -30,7 +30,38 @@ int isvowel(char c){
 }
 
 void tprint(struct tobj *t){
-	printf("%s %s %s.\n", t->state & BROKEN ? "a broken" : isvowel(t->desc[0]) ? "an" : "a", t->desc, t->name);
+	int adj = 0;
+	struct tobj *child = t->child;
+
+	/* Put indefinite article "a" */
+	printf("a");
+
+	/* Adjectives based off state */
+	if(t->state & BROKEN){
+		printf(" broken,");
+		++adj;
+	}
+	if(t->state & LOCKED){
+		printf(" locked,");
+		++adj;
+	}
+
+	/* Turn the indefinite article "a" into "an" if necessary */
+	if(adj == 0 && isvowel(t->desc[0])){
+		printf("n");
+	}
+
+	/* Description of object */
+	printf(" %s %s.\n", t->desc, t->name);
+
+	/* List of children */
+	if(child){
+		printf("There is");
+		do{
+			printf(!child->next && child != t->child ? " and %s %s " : " %s %s,", isvowel(child->name[0]) ? "an" : "a", child->name);
+		}while((child = child->next));	
+		printf("\b.\n");
+	}
 }
 
 int lookf(struct tobj *t){
@@ -44,6 +75,20 @@ int breakf(struct tobj *t){
 	return 1;	
 }
 
+
+int unlockf(struct tobj *t){
+	t->state &= ~LOCKED;
+	printf("You unlocked the %s.\n", t->name);	
+	return 1;	
+}
+
+
+int lockf(struct tobj *t){
+	t->state |= LOCKED;
+	printf("You locked the %s.\n", t->name);	
+	return 1;	
+}
+
 int tstrcmp(char *a, char *b){
 	while(*a && *b){
 		int cmp = tolower(*a++) - tolower(*b++);
@@ -54,16 +99,36 @@ int tstrcmp(char *a, char *b){
 	return *a - *b;
 }
 
+struct tobj *tfind(struct tobj *t, char *str){
+	if(tstrcmp(str, t->name) == 0){
+		return t;
+	}
+	t = t->child;
+	if(t == NULL){
+		return NULL;
+	}
+	do{
+		if(tstrcmp(str, t->name) == 0){
+			return t;
+		}
+	}while((t = t->next));
+
+	return NULL;
+}
+
 int main()
 {
-	struct tobj chair = {"chair", "wooden", 0, 0, NULL, NULL};
-	struct tobj bathroom = {"bathroom", "ugly run-down", 0, 0, &chair, NULL};
+	struct tobj trash = {"trashcan", "tin", 0, 0, NULL, NULL};
+	struct tobj pen = {"pen", "ball-point", 0, 0, NULL, NULL};
+	struct tobj desk = {"desk", "waferboard", 0, 0, &pen, &trash};
+	struct tobj chair = {"chair", "wooden", 0, 0, NULL, &desk};
+	struct tobj bathroom = {"bathroom", "ugly, run-down", CONTAINER, 0, &chair, NULL};
 	struct tobj *cloc = &bathroom;
 	using_history();
 	stifle_history(20);
 	while(1){
 		int fail = 0;
-		struct tobj *noun = cloc;
+		struct tobj *noun = NULL;
 		char *verbstr = readline(">> ");
 		char *temp = verbstr;
 		char *nounstr = verbstr;
@@ -80,22 +145,29 @@ int main()
 			++temp;
 		}
 
-		if(tstrcmp(nounstr, "chair") == 0){
-			noun = &chair;
-		}
-		else if(tstrcmp(nounstr, "bathroom") == 0){
-			noun = &bathroom;
-		}
-		else if(verbstr != nounstr){
-			printf("I don't see ``%s'' here.\n", nounstr);
-			fail = 1;
+		noun = tfind(cloc, nounstr);
+		if(noun == NULL){
+			if(verbstr != nounstr){
+				printf("I don't see ``%s'' here.\n", nounstr);
+				fail = 1;
+			}
+			else{
+				noun = cloc;
+			}
 		}
 		if(!fail){
 			if(tstrcmp(verbstr, "look") == 0){
+				printf(noun == cloc ? "You are in " : "You are looking at ");
 				lookf(noun);
 			}
 			else if(tstrcmp(verbstr, "break") == 0){
 				breakf(noun);
+			}
+			else if(tstrcmp(verbstr, "unlock") == 0){
+				unlockf(noun);
+			}
+			else if(tstrcmp(verbstr, "lock") == 0){
+				lockf(noun);
 			}
 			else{
 				printf("Don't know how to ``%s.''\n", verbstr);
