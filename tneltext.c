756eb2f2237e22beb1235ac696fb13d43d9c5738
tnelsond <tnelsond@gmail.com>
2015-08-22 20:18:09 -0500

    Refactored tfind and made everything a child or sibling of self

--- tneltext.c
+++ tneltext.c
@@ -13,12 +13,12 @@
 #define CONTAINER 4
 #define EDIBLE 8
 #define ROOM 16
+#define IN 32
 
 /* state */
 #define LOCKED 1
 #define BROKEN 2
-#define USEADJ 4
-#define DESCR  8
+#define DESCR  4
 
 struct tobj{
 	char *name;
@@ -29,8 +29,8 @@ struct tobj{
 	struct tobj *next;
 };
 
-struct tobj *cloc;
 struct tobj self = {"self", "\b\b\b", CONTAINER, 0, NULL, NULL};
+struct tobj nothing = {"", "", CONTAINER, 0, NULL, NULL};
 
 int isvowel(char c){
 	c = tolower(c);
@@ -39,23 +39,21 @@ int isvowel(char c){
 
 /* Print description */
 void tdescr(struct tobj *t, int which){
-	int adj = 0;
 	/* Put indefinite article "a" */
 	printf("a");
 
-	if(which & USEADJ){
+	if(which & DESCR){
+		int adj = 0;
 		/* Adjectives based off state */
 		if(t->state & LOCKED){
-			printf("%s locked", adj ? "," : "");
+			printf(" locked,");
 			++adj;
 		}
 		if(t->state & BROKEN){
-			printf("%s broken", adj ? "," : "");
+			printf(" broken,");
 			++adj;
 		}
-	}
-	
-	if(which & DESCR){
+
 		/* Turn the indefinite article "a" into "an" if necessary */
 		if(adj == 0 && isvowel(t->desc[0])){
 			printf("n");
@@ -64,7 +62,7 @@ void tdescr(struct tobj *t, int which){
 		printf(" %s %s.\n", t->desc, t->name);
 	}
 	else{
-		if(adj == 0 && isvowel(t->name[0])){
+		if(isvowel(t->name[0])){
 			printf("n");
 		}
 		printf(" %s; ", t->name);
@@ -76,7 +74,7 @@ void tprint(struct tobj *t, int printself, char *carryingstr){
 
 	/* Print itself */
 	if(printself){
-		tdescr(t, DESCR | USEADJ);
+		tdescr(t, DESCR);
 	}
 	
 	/* Print list of children */
@@ -86,20 +84,15 @@ void tprint(struct tobj *t, int printself, char *carryingstr){
 			if(!child->next && child != t->child){
 				printf("and ");
 			}
-			if(child->state & USEADJ){
-				tdescr(child, USEADJ);
-			}
-			else{
-				tdescr(child, 0);
-			}
+			tdescr(child, child->state);
 		}while((child = child->next));	
 		printf("\b\b.\n");
 	}
 }
 
 int lookf(struct tobj *t){
-	if(t == cloc){
-		printf("You are in a ");
+	if(t == self.next){
+		printf(self.next->state & IN ? "You are in " : "You are at ");
 	}
 	else{
 		printf("You are looking at ");
@@ -128,7 +121,7 @@ int unlockf(struct tobj *t){
 
 int gof(struct tobj *t){
 	if(t->type & ROOM){
-		cloc = t;
+		self.next = t;
 		lookf(t);
 		return 1;
 	}
@@ -169,39 +162,8 @@ int tstrequals(char *a, int num, ...){
 	return 0;
 }
 
-struct tobj *tfindprev(struct tobj *src, struct tobj *target){
-	src = src->child;
-	if(!src || !src->child){
-		return NULL;
-	}
-	src = src->child;
-	do{
-		if(src->next == target){
-			return src;
-		}
-	}while((src = src->next));
-	return NULL;
-}
-
-struct tobj *tfind(struct tobj *t, char *str){
-	if(tstrcmp(str, t->name) == 0){
-		return t;
-	}
-	else if(tstrcmp(str, "self") == 0){
-		return &self;
-	}
-	t = t->child;
-	if(!t){
-		return NULL;
-	}
-
-	do{
-		if(tstrcmp(str, t->name) == 0){
-			return t;
-		}
-	}while((t = t->next));
-
-	t = self.child;
+/* Returns the tobj that has the same name as str and points prev to the tobj that's either a parent or an older sister node to the returned tobj */
+struct tobj *tfind(struct tobj *t, struct tobj **prev, char *str){
 	if(!t){
 		return NULL;
 	}
@@ -209,24 +171,46 @@ struct tobj *tfind(struct tobj *t, char *str){
 		if(tstrcmp(str, t->name) == 0){
 			return t;
 		}
-	}while((t = t->next));
+		*prev = t;
+		struct tobj *ct = t->child;
+		if(ct){
+			do{
+				if(tstrcmp(str, ct->name) == 0){
+					return ct;
+				}
+				*prev = ct;
+				ct = ct->next;
+			}while(ct);
+		}
+		*prev = t;
+		t = t->next;	
+	}while(t);
 
 	return NULL;
 }
 
 void movef(struct tobj *prev, struct tobj *target, struct tobj *dest){
 	if(prev){
-		prev->next = target->next;
+		if(prev->next == target){
+			prev->next = target->next;
+		}
+		else{
+			prev->child = target->next;
+		}
 	}
 	target->next = dest->child;
 	dest->child = target;
 }
 
-int pickf(struct tobj *t){
+int eatf(struct tobj *prev, struct tobj *t){
+	movef(prev, t, &nothing);
+	printf("You ate the %s.\n", t->name);
+	return 0;
+}
+
+int pickf(struct tobj *prev, struct tobj *t){
 	if(t->type & PICKABLE){
-		/* Remove t from wherever it is and place it in the front of the inventory */
-		struct tobj *temp = tfindprev(cloc, t);
-		movef(temp, t, &self);
+		movef(prev, t, &self);
 		printf("You picked up the %s.\n", t->name);
 	}
 	else{
@@ -238,7 +222,7 @@ int pickf(struct tobj *t){
 
 int main()
 {
-	struct tobj brokenleg = {"leg", "\b", 0, USEADJ | BROKEN | LOCKED, NULL, NULL};
+	struct tobj brokenleg = {"leg", "\b\b", 0, DESCR | BROKEN | LOCKED, NULL, NULL};
 	struct tobj broccoli = {"broccoli", "yummy", EDIBLE, 0, NULL, &brokenleg};
 	self.child = &broccoli;
 	struct tobj trash = {"trashcan", "tin", CONTAINER | PICKABLE, 0, NULL, NULL};
@@ -246,11 +230,12 @@ int main()
 	struct tobj desk = {"desk", "waferboard", CONTAINER | ROOM, 0, &pen, &trash};
 	struct tobj chair = {"chair", "wooden", 0, 0, NULL, &desk};
 	struct tobj bathroom = {"bathroom", "ugly, run-down", CONTAINER | ROOM, 0, &chair, NULL};
-	cloc = &bathroom;
+	self.next = &bathroom;
 	using_history();
 stifle_history(20);
 	while(1){
 		int fail = 0;
+		struct tobj *prev = NULL;
 		struct tobj *noun = NULL;
 		char *verbstr = readline(">> ");
 		char *temp = verbstr;
@@ -267,22 +252,28 @@ stifle_history(20);
 			}
 			++temp;
 		}
-
-		noun = tfind(cloc, nounstr);
+		noun = tfind(&self, &prev, nounstr);
 		if(noun == NULL){
 			if(verbstr != nounstr){
 				printf("I don't see ``%s'' here.\n", nounstr);
 				fail = 1;
 			}
 			else{
-				noun = cloc;
+				prev = NULL;
+				noun = self.next;
 			}
 		}
 		if(!fail){
 			if(tstrcmp(verbstr, "look") == 0){
 				lookf(noun);
 			}
-			else if(tstrcmp(verbstr, "go") == 0){
+			else if(tstrequals(verbstr, 2, "i", "inventory")){
+				lookf(&self);
+			}
+			else if(tstrequals(verbstr, 3, "eat", "consume", "ingest")){
+				eatf(prev, noun);
+			}
+			else if(tstrequals(verbstr, 2, "go", "travel")){
 				gof(noun);
 			}
 			else if(tstrequals(verbstr, 5, "break", "smash", "kick", "punch", "headbutt")){
@@ -294,8 +285,8 @@ stifle_history(20);
 			else if(tstrequals(verbstr, 2, "lock", "bar")){
 				lockf(noun);
 			}
-			else if(tstrequals(verbstr, 2, "pick", "take")){
-				pickf(noun);
+			else if(tstrequals(verbstr, 4, "pick", "take", "steal", "pack")){
+				pickf(prev, noun);
 			}
 			else{
 				printf("Don't know how to ``%s.''\n", verbstr);
